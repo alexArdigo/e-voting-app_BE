8702390cb7bbbaf34fcb8ddaf2d13ae4bc47fe5e
Alexandre Ardigo <alexaardigo@gmail.com>
2025-06-21 22:25:31 +0100

    Add Voter entity with associations to ElectoralCircle and Parish

--- src/main/java/com/iscte_meta_systems/evoting_server/entities/Voter.java
+++ src/main/java/com/iscte_meta_systems/evoting_server/entities/Voter.java
@@ -1,4 +1,57 @@
 package com.iscte_meta_systems.evoting_server.entities;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+
+@Entity
 public class Voter {
+
+    @Id
+    @GeneratedValue
+    Long id;
+    String hashIdentification;
+
+    @OneToOne
+    ElectoralCircle electoralCircle;
+
+    @OneToOne
+    Parish parish;
+
+    public Voter() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getHashIdentification() {
+        return hashIdentification;
+    }
+
+    public void setHashIdentification(String hashIdentification) {
+        this.hashIdentification = hashIdentification;
+    }
+
+    public ElectoralCircle getElectoralCircle() {
+        return electoralCircle;
+    }
+
+    public void setElectoralCircle(ElectoralCircle electoralCircle) {
+        this.electoralCircle = electoralCircle;
+    }
+
+    public Parish getParish() {
+        return parish;
+    }
+
+    public void setParish(Parish parish) {
+        this.parish = parish;
+    }
 }
