b0da1e80ba90ff8b2d17e7be91af1310c53122e4
tiago <tamsa2@iscte-iul.pt>
2025-06-21 22:11:14 +0100

    Add getters and setters for Candidate properties

--- src/main/java/com/iscte_meta_systems/evoting_server/entities/Candidate.java
+++ src/main/java/com/iscte_meta_systems/evoting_server/entities/Candidate.java
@@ -13,4 +13,33 @@ public class Candidate {
     private String name;
     private String imageUrl;
 
+    public Candidate() {}
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+
 }
+
+
