61ab734188a992f9261bc57c6b084f9fe078bfbb
rita_santos <rassa2@iscte-iul.pt>
2025-06-21 16:13:53 +0100

    Add Party and UniParty entities with attributes and inheritance from Organisation

--- src/main/java/com/iscte_meta_systems/evoting_server/entities/Party.java
+++ src/main/java/com/iscte_meta_systems/evoting_server/entities/Party.java
@@ -1,4 +1,55 @@
 package com.iscte_meta_systems.evoting_server.entities;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 
-public class Party {
+@Entity
+public class Party extends Organisation {
+
+    private String name;
+    private String color;
+    private String logoUrl;
+    private String description;
+    //@OneToMany
+    //List<Candidate> candidates;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getLogoUrl() {
+        return logoUrl;
+    }
+
+    public void setLogoUrl(String logoUrl) {
+        this.logoUrl = logoUrl;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+//    public List<Candidate> getCandidates() {
+//        return candidates;
+//    }
+//
+//    public void setCandidates(List<Candidate> candidates) {
+//        this.candidates = candidates;
+//    }
 }
