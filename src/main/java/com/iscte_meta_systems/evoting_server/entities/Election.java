b96dded66a4aed22c8deb661ece83b11677da961
Fernando Rocha <feara@iscte-iul.pt>
2025-06-21 12:48:12 +0100

    Add fields for name, description, startDate, and endDate to Election entity

--- src/main/java/com/iscte_meta_systems/evoting_server/entities/Election.java
+++ src/main/java/com/iscte_meta_systems/evoting_server/entities/Election.java
@@ -1,6 +1,6 @@
 package com.iscte_meta_systems.evoting_server.entities;
-
 import jakarta.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 @Inheritance(strategy = InheritanceType.JOINED)
@@ -9,6 +9,14 @@ public class Election {
     @Id
     @GeneratedValue
     private Long id;
+    String name;
+    String description;
+    LocalDateTime startDate;
+    LocalDateTime endDate;
+    //List<Results> results;
+    //List<Vote> votes;
+    //List<Hash> voted
+
 
     public void setId(Long id) {
         this.id = id;
