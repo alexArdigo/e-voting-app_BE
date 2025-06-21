4710db909727b552402a118dc55d1f294c78b4d9
rita_santos <rassa2@iscte-iul.pt>
2025-06-21 15:58:51 +0100

    Add Organisation entity with inheritance and ID field

--- src/main/java/com/iscte_meta_systems/evoting_server/entities/Organisation.java
+++ src/main/java/com/iscte_meta_systems/evoting_server/entities/Organisation.java
@@ -1,4 +1,19 @@
 package com.iscte_meta_systems.evoting_server.entities;
+import jakarta.persistence.*;
 
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
 public class Organisation {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
