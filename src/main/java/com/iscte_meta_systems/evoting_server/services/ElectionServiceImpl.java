2183a1b7d312542691c77510760430fd0679e7ee
Fernando Rocha <feara@iscte-iul.pt>
2025-06-21 14:43:55 +0100

    Remove unused PresidentialRepository import in ElectionServiceImpl

--- src/main/java/com/iscte_meta_systems/evoting_server/services/ElectionServiceImpl.java
+++ src/main/java/com/iscte_meta_systems/evoting_server/services/ElectionServiceImpl.java
@@ -2,7 +2,6 @@ package com.iscte_meta_systems.evoting_server.services;
 
 import com.iscte_meta_systems.evoting_server.entities.Election;
 import com.iscte_meta_systems.evoting_server.repositories.ElectionRepository;
-import com.iscte_meta_systems.evoting_server.repositories.PresidentialRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
