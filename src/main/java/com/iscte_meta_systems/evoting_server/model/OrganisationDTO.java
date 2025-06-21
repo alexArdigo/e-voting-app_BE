49d007f6f94bac4d255fc9c17bf86b223f81cf9c
rita_santos <rassa2@iscte-iul.pt>
2025-06-21 20:36:23 +0100

    Add OrganisationDTO class with fields and accessors

---
+++ src/main/java/com/iscte_meta_systems/evoting_server/model/OrganisationDTO.java
@@ -0,0 +1,40 @@
+package com.iscte_meta_systems.evoting_server.model;
+
+public class OrganisationDTO {
+    private Long id;
+    private String organisationType;
+    private String name;
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getOrganisationType() {
+        return organisationType;
+    }
+
+    public void setOrganisationType(String organisationType) {
+        this.organisationType = organisationType;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
