40d9d2745e3dcab4c7aa2cc4d90e632ce3b276fa
Fernando Rocha <feara@iscte-iul.pt>
2025-06-21 12:46:56 +0100

    Add id field to Election entity with getter and setter methods

--- src/main/java/com/iscte_meta_systems/evoting_server/entities/Election.java
+++ src/main/java/com/iscte_meta_systems/evoting_server/entities/Election.java
@@ -1,4 +1,20 @@
 package com.iscte_meta_systems.evoting_server.entities;
 
+import jakarta.persistence.*;
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
 public class Election {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
 }
