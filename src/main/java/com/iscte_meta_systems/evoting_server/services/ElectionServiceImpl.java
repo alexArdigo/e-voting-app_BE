3371be1e9e097e5eba7d012760d6c03cdf305781
Fernando Rocha <feara@iscte-iul.pt>
2025-06-21 12:43:58 +0100

    Add ElectionRepository and integrate it into ElectionServiceImpl

--- src/main/java/com/iscte_meta_systems/evoting_server/services/ElectionServiceImpl.java
+++ src/main/java/com/iscte_meta_systems/evoting_server/services/ElectionServiceImpl.java
@@ -1,12 +1,17 @@
 package com.iscte_meta_systems.evoting_server.services;
 
 import com.iscte_meta_systems.evoting_server.entities.Election;
+import com.iscte_meta_systems.evoting_server.repositories.ElectionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class ElectionServiceImpl implements  ElectionService {
+
+    @Autowired
+    private ElectionRepository electionRepository;
 
     @Override
     public List<Election> getElections() {
