088a4b35a9a3b3defe92b1d5ad8068ce2e75e29a
Alexandre Ardigo <alexaardigo@gmail.com>
2025-06-22 09:25:03 +0100

    Refactor VoterService to include electionId in hasAlreadyVoted method and update VoterServiceImpl implementation

--- src/main/java/com/iscte_meta_systems/evoting_server/services/VoterService.java
+++ src/main/java/com/iscte_meta_systems/evoting_server/services/VoterService.java
@@ -1,10 +1,10 @@
 package com.iscte_meta_systems.evoting_server.services;
 
+import com.iscte_meta_systems.evoting_server.entities.Election;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
 
-@Service
 public interface VoterService {
 
-    Boolean hasAlreadyVoted(Long voter);
+    Boolean hasAlreadyVoted(String voter, Long electionId);
 }
--- src/main/java/com/iscte_meta_systems/evoting_server/services/VoterServiceImpl.java
+++ src/main/java/com/iscte_meta_systems/evoting_server/services/VoterServiceImpl.java
@@ -1,11 +1,25 @@
 package com.iscte_meta_systems.evoting_server.services;
 
-import org.springframework.http.HttpStatusCode;
+import com.iscte_meta_systems.evoting_server.entities.Election;
+import com.iscte_meta_systems.evoting_server.repositories.ElectionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class VoterServiceImpl implements VoterService {
 
+    @Autowired
+    private ElectionRepository electionRepository;
+
     @Override
-    public Boolean hasAlreadyVoted(Long voter) {
+    public Boolean hasAlreadyVoted(String voter, Long electionId) {
+        if (voter == null || electionId == null)
+            throw new NullPointerException();
+        /*
+        Election election = electionRepository.findElectionById(electionId);
+
+        return election.getVotedList().stream().allMatch(hash -> hash.equals(voter));
+        */
         return null;
     }
 }
