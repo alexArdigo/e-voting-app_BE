e732397414272be3ae853cd657e6bd6cfe4c72ff
Alexandre Ardigo <alexaardigo@gmail.com>
2025-06-22 09:27:26 +0100

    Update VoterController to include electionId in hasAlreadyVoted endpoint and return ResponseEntity

--- src/main/java/com/iscte_meta_systems/evoting_server/controllers/VoterController.java
+++ src/main/java/com/iscte_meta_systems/evoting_server/controllers/VoterController.java
@@ -1,7 +1,9 @@
 package com.iscte_meta_systems.evoting_server.controllers;
 
+import com.iscte_meta_systems.evoting_server.entities.Election;
 import com.iscte_meta_systems.evoting_server.services.VoterService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +15,7 @@ public class VoterController {
     private VoterService voterService;
 
     @GetMapping("/voters/has-voted")
-    public Boolean hasAlreadyVoted(@RequestBody Long voter) {
-        return voterService.hasAlreadyVoted(voter);
+    public ResponseEntity<?> hasAlreadyVoted(@RequestBody String voter, Long electionId) {
+        return ResponseEntity.ok().body(voterService.hasAlreadyVoted(voter, electionId));
     }
 }
