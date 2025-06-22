71468344cfdd5a623a8e815ace25d966f372ecf9
Alexandre Ardigo <alexaardigo@gmail.com>
2025-06-22 10:00:48 +0100

    Implement initial Vote entity with id and organisation fields

---
+++ src/main/java/com/iscte_meta_systems/evoting_server/entities/Vote.java
@@ -0,0 +1,38 @@
+package com.iscte_meta_systems.evoting_server.entities;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+
+@Entity
+public class Vote {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @OneToOne
+    private Organisation organisation;
+    //private Parish parish;
+
+
+    public Vote() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Organisation getOrganisation() {
+        return organisation;
+    }
+
+    public void setOrganisation(Organisation organisation) {
+        this.organisation = organisation;
+    }
+}
