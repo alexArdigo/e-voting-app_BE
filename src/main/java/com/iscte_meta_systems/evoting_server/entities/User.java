eb77a7abd6adbcf07e230ce774710640b69a030e
Sara Sousa <ssaaa3@iscte-iul.pt>
2025-06-22 16:07:51 +0100

    Implement User entity

--- src/main/java/com/iscte_meta_systems/evoting_server/entities/User.java
+++ src/main/java/com/iscte_meta_systems/evoting_server/entities/User.java
@@ -1,4 +1,68 @@
 package com.iscte_meta_systems.evoting_server.entities;
+import jakarta.persistence.*;
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
 
 public class User {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    Long id;
+
+    String username;
+    String password;
+    String name;
+    Boolean isAuth;
+
+    public User() {
+    }
+
+    public User(String username, String password, String name) {
+        this.username = username;
+        this.password = password;
+        this.name = name;
+        this.isAuth = false; // Default value
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Boolean getAuth() {
+        return isAuth;
+    }
+
+    public void setAuth(Boolean auth) {
+        isAuth = auth;
+    }
 }
+
