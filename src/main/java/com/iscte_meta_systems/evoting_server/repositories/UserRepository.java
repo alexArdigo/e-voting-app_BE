1a2cee93ce89a26949be6d31348ffe4aa2817372
Sara Sousa <ssaaa3@iscte-iul.pt>
2025-06-22 17:05:24 +0100

    Add UserRepository interface with methods for username lookup

--- src/main/java/com/iscte_meta_systems/evoting_server/repositories/UserRepository.java
+++ src/main/java/com/iscte_meta_systems/evoting_server/repositories/UserRepository.java
@@ -1,4 +1,9 @@
 package com.iscte_meta_systems.evoting_server.repositories;
 
-public class UserRepository {
+import com.iscte_meta_systems.evoting_server.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+    boolean existsByUsername(String username);
 }
