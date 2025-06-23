aed4bf51290aea3b017c08399df42591bdb2473a
Fernando Rocha <feara@iscte-iul.pt>
2025-06-23 17:45:01 +0100

    Integrate `UserAuthenticationProvider` with `UserService` and password encryption. Add `PasswordEncoderConfig` for bcrypt password encoding.

--- src/main/java/com/iscte_meta_systems/evoting_server/UserAuthenticationProvider.java
+++ src/main/java/com/iscte_meta_systems/evoting_server/UserAuthenticationProvider.java
@@ -1,23 +1,50 @@
 package com.iscte_meta_systems.evoting_server;
 
+import com.iscte_meta_systems.evoting_server.entities.User;
+import com.iscte_meta_systems.evoting_server.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Component
 public class UserAuthenticationProvider implements AuthenticationProvider {
 
+    @Autowired
+    private UserService userService;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
         String username = authentication.getName();
         String password = authentication.getCredentials().toString();
 
-        //User user = authService.validateLogin(username, password);
+        User user = userService.getUserByUsername(username);
+
+        if (user == null || !user.getIsAuthorized()) {
+            throw new BadCredentialsException("User not found or not authorized");
+        }
+
+        if (!passwordEncoder.matches(password, user.getPassword())) {
+            throw new BadCredentialsException("Invalid password");
+        }
+
+        List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority("ROLE_" + user.getRole()));
+
+        return new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword(), authorities);
+
         /*if (user != null) {
             List<GrantedAuthority> roles = new ArrayList<>();
             roles.add(new SimpleGrantedAuthority("ROLE_USER"));
@@ -28,7 +55,6 @@ public class UserAuthenticationProvider implements AuthenticationProvider {
         }
 
          */
-        return null;
     }
 
     @Override
