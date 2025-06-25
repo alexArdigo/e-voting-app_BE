ec9470bcffd65664a1a6e9bccb4942e449194d7c
Fernando Rocha <feara@iscte-iul.pt>
2025-06-25 11:04:01 +0100

    Extend `HelpCommentRepository` with JpaRepository to enable database operations.

--- src/main/java/com/iscte_meta_systems/evoting_server/repositories/HelpCommentRepository.java
+++ src/main/java/com/iscte_meta_systems/evoting_server/repositories/HelpCommentRepository.java
@@ -1,7 +1,9 @@
 package com.iscte_meta_systems.evoting_server.repositories;
 
+import com.iscte_meta_systems.evoting_server.entities.HelpComment;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface HelpCommentRepository {
+public interface HelpCommentRepository extends JpaRepository<HelpComment, Long> {
 }
