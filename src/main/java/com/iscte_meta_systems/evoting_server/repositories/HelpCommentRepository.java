3694380a1937d97a183b304fb1202370ca3fb4a6
Fernando Rocha <feara@iscte-iul.pt>
2025-06-25 11:03:05 +0100

    Introduce `HelpCommentRepository` and inject into `CommentServiceImpl`.

---
+++ src/main/java/com/iscte_meta_systems/evoting_server/repositories/HelpCommentRepository.java
@@ -0,0 +1,7 @@
+package com.iscte_meta_systems.evoting_server.repositories;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface HelpCommentRepository {
+}
--- src/main/java/com/iscte_meta_systems/evoting_server/services/CommentServiceImpl.java
+++ src/main/java/com/iscte_meta_systems/evoting_server/services/CommentServiceImpl.java
@@ -1,6 +1,7 @@
 package com.iscte_meta_systems.evoting_server.services;
 
 import com.iscte_meta_systems.evoting_server.entities.HelpComment;
+import com.iscte_meta_systems.evoting_server.repositories.HelpCommentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
