b24b3bbcede25ad5f00c4d01683b0cb71ad5c34c
rita_santos <rassa2@iscte-iul.pt>
2025-06-26 12:03:30 +0100

    Add likeCount field and related methods to HelpCommentDTO for tracking likes

--- src/main/java/com/iscte_meta_systems/evoting_server/model/HelpCommentDTO.java
+++ src/main/java/com/iscte_meta_systems/evoting_server/model/HelpCommentDTO.java
@@ -1,4 +1,50 @@
 package com.iscte_meta_systems.evoting_server.model;
 
 public class HelpCommentDTO {
+    private Long id;
+    private String comment;
+    private String answer;
+    private int likeCount;
+
+    public HelpCommentDTO() {
+    }
+
+    public HelpCommentDTO(Long id, String comment, String answer, int likeCount) {
+        this.id = id;
+        this.comment = comment;
+        this.answer = answer;
+        this.likeCount = likeCount;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public int getLikeCount() {
+        return likeCount;
+    }
+
+    public void setLikeCount(int likeCount) {
+        this.likeCount = likeCount;
+    }
 }
