35f7a2b1aeaad8b24a1cf7b4fdf1f4eeb816d0a5
Sara Sousa <ssaaa3@iscte-iul.pt>
2025-06-26 20:48:49 +0100

    Add endpoint to retrieve vote percentages by party and district

--- src/main/java/com/iscte_meta_systems/evoting_server/controllers/StatisticsController.java
+++ src/main/java/com/iscte_meta_systems/evoting_server/controllers/StatisticsController.java
@@ -1,4 +1,24 @@
 package com.iscte_meta_systems.evoting_server.controllers;
 
+import com.iscte_meta_systems.evoting_server.model.PartyVoteStatsDTO;
+import com.iscte_meta_systems.evoting_server.services.StatisticsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class StatisticsController {
+
+    @Autowired
+    StatisticsService statisticsService;
+
+    @GetMapping("/vote-percentages") //vote-percentages?electionId=1&districtName=Aveiro
+    public List<PartyVoteStatsDTO> getStats(
+            @RequestParam Long electionId,
+            @RequestParam String districtName) {
+        return statisticsService.getVotePercentagesByPartyByDistrict(electionId, districtName);
+    }
 }
