2079ac72c99612669bd6d8d5ee4b57f1cce7481e
Sara Sousa <ssaaa3@iscte-iul.pt>
2025-06-26 10:56:37 +0100

    Add StatisticsService interface for election-related statistics retrieval

---
+++ src/main/java/com/iscte_meta_systems/evoting_server/services/StatisticsService.java
@@ -0,0 +1,22 @@
+package com.iscte_meta_systems.evoting_server.services;
+
+public interface StatisticsService {
+
+    int getTotalVotes(Long electionId);
+
+    int getTotalVoters(Long electionId);
+
+    int getTotalElections();
+
+    int getTotalVotersByMunicipality(Long electionId, String municipality);
+
+    int getTotalVotersByDistrict(Long electionId, String district);
+
+    int getTotalVotersByParish(Long electionId, String parish);
+
+    int getTotalVotersByUniparty(Long electionId, String unipartyName);
+
+    int getTotalVotersByParty(Long electionId, String partyName);
+
+
+}
