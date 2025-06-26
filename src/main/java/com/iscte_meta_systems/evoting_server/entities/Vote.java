dc20cb5adbe5aa2e368db8db0e3b4d31b64ecd5f
Sara Sousa <ssaaa3@iscte-iul.pt>
2025-06-26 13:19:03 +0100

    StatisticsService interface for vote statistics

--- src/main/java/com/iscte_meta_systems/evoting_server/entities/Vote.java
+++ src/main/java/com/iscte_meta_systems/evoting_server/entities/Vote.java
@@ -1,10 +1,7 @@
 package com.iscte_meta_systems.evoting_server.entities;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.*;
 
 @Entity
 public class Vote {
@@ -14,6 +11,7 @@ public class Vote {
 
     @OneToOne
     private Organisation organisation;
+
     @OneToOne
     private Parish parish;
 
--- src/main/java/com/iscte_meta_systems/evoting_server/repositories/VoteRepository.java
+++ src/main/java/com/iscte_meta_systems/evoting_server/repositories/VoteRepository.java
@@ -6,4 +6,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface VoteRepository extends JpaRepository<Vote, Long> {
+
+
+
 }
