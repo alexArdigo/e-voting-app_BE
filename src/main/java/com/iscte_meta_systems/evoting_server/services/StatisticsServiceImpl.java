4c90d55b533fe025d083593d5f3cdf899545efcf
tiago <tamsa2@iscte-iul.pt>
2025-06-27 15:06:43 +0100

    Add @Service annotation to StatisticsServiceImpl

--- src/main/java/com/iscte_meta_systems/evoting_server/services/StatisticsServiceImpl.java
+++ src/main/java/com/iscte_meta_systems/evoting_server/services/StatisticsServiceImpl.java
@@ -8,12 +8,14 @@ import com.iscte_meta_systems.evoting_server.model.PartyVoteDTO;
 import com.iscte_meta_systems.evoting_server.repositories.DistrictRepository;
 import com.iscte_meta_systems.evoting_server.repositories.VoteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+@Service
 public class StatisticsServiceImpl implements StatisticsService {
     @Autowired
     private VoteRepository voteRepository;
