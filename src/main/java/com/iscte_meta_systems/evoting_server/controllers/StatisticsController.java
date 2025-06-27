515d0e6ea39d52e99f80e175325df005598e7364
tiago <tamsa2@iscte-iul.pt>
2025-06-27 16:11:49 +0100

    Refactor StatisticsController to include DistrictStatisticsDTO and PartyVoteStatsDTO imports

--- src/main/java/com/iscte_meta_systems/evoting_server/controllers/StatisticsController.java
+++ src/main/java/com/iscte_meta_systems/evoting_server/controllers/StatisticsController.java
@@ -1,8 +1,12 @@
 package com.iscte_meta_systems.evoting_server.controllers;
 
+import com.iscte_meta_systems.evoting_server.model.DistrictStatisticsDTO;
+import com.iscte_meta_systems.evoting_server.model.PartyVoteStatsDTO;
 import com.iscte_meta_systems.evoting_server.services.StatisticsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -12,7 +16,6 @@ public class StatisticsController {
 
     @Autowired
     private StatisticsService statisticsService;
-    StatisticsService statisticsService;
 
     @GetMapping("/vote-percentages") //vote-percentages?electionId=1&districtName=Aveiro
     public List<PartyVoteStatsDTO> getStats(
