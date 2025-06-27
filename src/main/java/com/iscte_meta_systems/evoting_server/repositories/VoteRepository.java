034e7983ecce331c71a0b993cae1ca69fbb49114
tiago <tamsa2@iscte-iul.pt>
2025-06-27 16:30:28 +0100

    Add custom query to VoteRepository for retrieving votes by district name

--- src/main/java/com/iscte_meta_systems/evoting_server/repositories/VoteRepository.java
+++ src/main/java/com/iscte_meta_systems/evoting_server/repositories/VoteRepository.java
@@ -2,6 +2,8 @@ package com.iscte_meta_systems.evoting_server.repositories;
 
 import com.iscte_meta_systems.evoting_server.entities.Vote;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
@@ -9,6 +11,6 @@ import java.util.List;
 public interface VoteRepository extends JpaRepository<Vote, Long> {
 
     List<Vote> findByElectionId(Long electionId);
-
+    @Query("SELECT v FROM Vote v WHERE v.parish.municipality.district.districtName = :districtName")
     List<Vote> findByDistrictName(String districtName);
 }
