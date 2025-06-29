086e66bbe88d7f3516e4b6feb3822dc27390154d
Sara Sousa <ssaaa3@iscte-iul.pt>
2025-06-29 22:31:20 +0100

    Update CORS configuration in SecurityWebConfig to allow specific origins

--- src/main/java/com/iscte_meta_systems/evoting_server/security/SecurityWebConfig.java
+++ src/main/java/com/iscte_meta_systems/evoting_server/security/SecurityWebConfig.java
@@ -20,29 +20,29 @@ public class SecurityWebConfig {
     @Autowired
     UserAuthenticationProvider userAuthenticationProvider;
 
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(List.of("*")); // allows all origins
-        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(List.of("*"));
-        configuration.setAllowCredentials(true); // still allows credentials like cookies
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
-
 //    @Bean
 //    public CorsConfigurationSource corsConfigurationSource() {
 //        CorsConfiguration configuration = new CorsConfiguration();
-//        configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:5174"));
+//        configuration.setAllowedOriginPatterns(List.of("*")); // allows all origins
 //        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 //        configuration.setAllowedHeaders(List.of("*"));
-//        configuration.setAllowCredentials(true);
+//        configuration.setAllowCredentials(true); // still allows credentials like cookies
 //        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 //        source.registerCorsConfiguration("/**", configuration);
 //        return source;
 //    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:5174"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 
 
     @Bean
