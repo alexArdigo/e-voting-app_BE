733c9208878847524527e8259ffd16e252bd5c40
Alexandre Ardigo <alexaardigo@gmail.com>
2025-07-08 23:24:30 +0100

    Refactor VotingSessionRepository: remove unused imports and add @Repository annotation

--- src/main/java/com/iscte_meta_systems/evoting_server/repositories/VotingSessionRepository.java
+++ src/main/java/com/iscte_meta_systems/evoting_server/repositories/VotingSessionRepository.java
@@ -1,13 +1,11 @@
 package com.iscte_meta_systems.evoting_server.repositories;
 
-import com.iscte_meta_systems.evoting_server.entities.Election;
-import com.iscte_meta_systems.evoting_server.entities.Voter;
 import com.iscte_meta_systems.evoting_server.entities.VotingSession;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface VotingSessionRepository extends JpaRepository<VotingSession, Long> {
-    VotingSession findByElectionAndVoter(Election election, Voter voter);
 
     VotingSession findByElectionIdAndVoterId(Long electionId, Long voterId);
 }
