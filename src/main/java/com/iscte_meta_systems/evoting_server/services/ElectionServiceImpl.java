b495f336b2320063dcaf88feb224653f2910989f
tiago <tamsa2@iscte-iul.pt>
2025-07-08 09:50:09 +0100

    Update ElectionServiceImpl and InitializeDBInjectionImpl: set legislative dateTime and remove unused party population call

--- src/main/java/com/iscte_meta_systems/evoting_server/services/ElectionServiceImpl.java
+++ src/main/java/com/iscte_meta_systems/evoting_server/services/ElectionServiceImpl.java
@@ -176,6 +176,7 @@ public class ElectionServiceImpl implements ElectionService {
 
                 legislative.setElectoralCircles(circles);
                 legislativeRepository.save(legislative);
+                legislative.setDateTime(startDate);
 
                 ElectionDTO legislativeResult = new ElectionDTO();
                 legislativeResult.setName(baseElection.getName());
