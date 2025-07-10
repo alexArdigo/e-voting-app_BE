4792d1083017397c9546636e550e3d364187228e
Alexandre Ardigo <alexaardigo@gmail.com>
2025-07-10 08:21:30 +0100

    Refactor OAuth authentication: update methods to handle OAuthToken, streamline voter retrieval, and enhance voter entity with OAuthToken association

--- src/main/java/com/iscte_meta_systems/evoting_server/repositories/OAuthTokenRepository.java
+++ src/main/java/com/iscte_meta_systems/evoting_server/repositories/OAuthTokenRepository.java
@@ -1,11 +1,7 @@
 package com.iscte_meta_systems.evoting_server.repositories;
 
 import com.iscte_meta_systems.evoting_server.entities.OAuthToken;
-import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -13,8 +9,5 @@ public interface OAuthTokenRepository extends JpaRepository<OAuthToken, Long> {
 
     OAuthToken findOAuthTokenByToken(String token);
 
-    @Modifying
-    @Transactional
-    @Query("DELETE FROM OAuthToken o WHERE o.token = :token")
-    void deleteByToken(@Param("token") String token);
+    void deleteByToken(String token);
 }
--- src/main/java/com/iscte_meta_systems/evoting_server/services/OAuthService.java
+++ src/main/java/com/iscte_meta_systems/evoting_server/services/OAuthService.java
@@ -1,15 +1,16 @@
 package com.iscte_meta_systems.evoting_server.services;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.iscte_meta_systems.evoting_server.entities.OAuthToken;
 
 import java.util.Map;
 
 public interface OAuthService {
     Map<String, String> authUrl();
 
-    Long addVoter(JsonNode userInfo, String token);
+    Long addVoter(JsonNode userInfo, OAuthToken token);
 
-    void authWithToken(String token, Long voterId);
+    void authWithToken(String token);
 
     Long callback(JsonNode payload);
 }
