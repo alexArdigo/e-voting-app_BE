12404423d2d8018a2152bd9a9262cc9f4fe2abd5
Fernando Rocha <feara@iscte-iul.pt>
2025-07-13 17:30:03 +0100

    Update `Presidential` entity: add cascade operations and orphan removal for candidates list

--- src/main/java/com/iscte_meta_systems/evoting_server/entities/Presidential.java
+++ src/main/java/com/iscte_meta_systems/evoting_server/entities/Presidential.java
@@ -1,5 +1,6 @@
 package com.iscte_meta_systems.evoting_server.entities;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
 
@@ -7,8 +8,8 @@ import java.util.List;
 
 @Entity
 public class Presidential extends Election {
-    @OneToMany
-    List<UniParty> candidates;
+    @OneToMany(mappedBy = "election", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<UniParty> candidates;
 
     public List<UniParty> getCandidates() {
         return candidates;
